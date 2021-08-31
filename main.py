a74a7a63be7a783ad87dbd8fbf1afd835d33e8c3
joeyjpowers <70409627+joeyjpowers@users.noreply.github.com>
2021-08-31 18:55:41 +0000

    Connected to Heroku + change discord token var name

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ async def on_message(message):
       await message.channel.send("Welcome to the Show Bot. Here is a guide to the current commands.\n - !stat: type !stat followed by a player name, their card's series name, and a stat name to find out any stat from any card!\n - !statlist: gives a list of all possible stat keywords\n - !series: type !series followed by a player name to find how many cards a player has and what series they are!\nMore commands will be added in the future so stay tuned!")
                 
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('DISCORD_TOKEN'))
